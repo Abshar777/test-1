7a6e6957eeaa1b82523cca70ee350f3b48d11842
Abshar777 <absharameen625@gmail.com>
2025-06-18 17:08:57 +0530

    feat: server added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fs from "fs";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // optional, if you want to receive JSON body
 
